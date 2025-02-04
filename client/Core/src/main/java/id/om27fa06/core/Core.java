90f74034b6c57146612d3cd02fe06adda54c5a31
om27fa06 <om27fa06@gmail.com>
2025-02-04 11:20:36 +0330

    Java UI is started

---
+++ client/Core/src/main/java/id/om27fa06/core/Core.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package id.om27fa06.core;
+
+/**
+ *
+ * @author Omar
+ */
+public class Core {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
